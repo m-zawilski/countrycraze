58790a24d8a036d9b1c7255f859bd5339e514143
m-zawilski <mzawilski1@gmail.com>
2019-08-10 01:29:40 +0200

    no sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
-import "./Components/App.scss";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
